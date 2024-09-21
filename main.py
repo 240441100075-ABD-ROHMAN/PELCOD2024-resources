cfa21cd452c5c69a848b906826a256f07e84befa
240441100075-ABD-ROHMAN <rohmanabd293@gmail.com>
2024-09-21 15:17:42 +0700

    commit ke-2

---
+++ main.py
@@ -0,0 +1,37 @@
+Nilai = int(input("Masukkan nilai: "))
+print(Nilai)
+
+if (Nilai > 90 and Nilai <= 100):
+     print("Selamat anda lulus, denagn nilai yang memuaskan")
+elif (Nilai > 80):
+     print("Selamat anda lulus, dengan nilai sangat memuaskan")
+elif (Nilai > 70):
+     print("lulus tapi remidi")
+else:
+     print("Anda masih belum lulus tes, semangat dan coba lagi")
+ 
+
+
+# for loop
+for i in range(10, 1, -2):
+     print(i)
+     print("Hello world")
+
+# while loop
+angka = 30
+
+while (angka > 1):
+    print(angka)
+
+
+# [FUNCTION]
+# def
+def sayhello():
+     print("Hello Rohman")
+
+sayhello()
+
+
+
+
+
